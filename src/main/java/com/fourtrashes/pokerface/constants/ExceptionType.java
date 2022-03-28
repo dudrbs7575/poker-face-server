5d27758cc1d04c958e2ea67764820a4bc0752736
김한성 <khs960616>
2022-03-28 22:10:59 +0900

    feature room service

--- src/main/java/com/fourtrashes/pokerface/constants/ExceptionType.java
+++ src/main/java/com/fourtrashes/pokerface/constants/ExceptionType.java
@@ -1,7 +1,10 @@
 package com.fourtrashes.pokerface.constants;
 
 public enum ExceptionType {
-    NOT_FOUNT_RESOURCE("E001", "데이터가 존재하지 않습니다.");
+    NOT_FOUNT_RESOURCE("E001", "데이터가 존재하지 않습니다."),
+    NOT_EXIST_ROOM("R001", "존재하지 않는 방입니다."),
+    ALREADY_FULL_ROOM("R002", "이미 사용자가 가득찬 방입니다."),
+    ALREADY_EXIT_ROOM("R003", "이미 종료된 방입니다.");
     private String code;
     private String message;
 
