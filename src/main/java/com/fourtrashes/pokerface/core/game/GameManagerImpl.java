db21904659905aab6335edc8e27245a76fe4481c
119wjw <119wjw@naver.com>
2022-03-29 15:05:19 +0900

    feat : GameManager 초기 구상

--- src/main/java/com/fourtrashes/pokerface/core/game/GameManagerImpl.java
+++ src/main/java/com/fourtrashes/pokerface/core/game/GameManagerImpl.java
@@ -1,25 +1,60 @@
 package com.fourtrashes.pokerface.core.game;
 
-public class GameManagerImpl implements GameManager{
+public class GameManagerImpl implements GameManager {
 
     // CardDeck은 singleton으로 관리되면 안됌
     CardDeck cardDeck = new CardDeckImpl();
 
     @Override
     public void startGame() {
+
+        checkReady();
+
         cardDeck.shuffle();
-        int i = 0;
-        while(i < 7){
-        /*
-            TODO 게임로직 개발
-                - 게임이 끝난 이후 endGame 호출
-        */
-            setTimer();
-            deal();
-            bet();
-            i++;
-        }
+
+        // 카드 4장씩 나누어주고 한장 버리는 로직
+        firstDeal();
+
+        // 배팅
+        bet();
+
+        // 4번째 카드
+        deal();
+
+        // 배팅
+        bet();
+
+        // 5번째 카드
+        deal();
+
+        // 배팅
+        bet();
+
+        // 6번째 카드
+        deal();
+
+        // 배팅
+        bet();
+
+        // 히든 카드
+        lastDeal();
+
+        // 배팅
+        bet();
+
         endGame();
+    }
+
+    public void checkReady() {
+
+    }
+
+    public void firstDeal() {
+
+    }
+
+    public void lastDeal(){
+
     }
 
     @Override
