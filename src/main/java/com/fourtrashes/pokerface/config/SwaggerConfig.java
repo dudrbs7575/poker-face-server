7c40e0319c0aedd0e717f2e9018b9554519a849b
김한성 <khs960616>
2022-03-29 11:32:18 +0900

    feature_swagger_config

--- src/main/java/com/fourtrashes/pokerface/config/SwaggerConfig.java
+++ src/main/java/com/fourtrashes/pokerface/config/SwaggerConfig.java
@@ -2,18 +2,34 @@ package com.fourtrashes.pokerface.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//
-//    }
-//
-//
-//    private ApiInfo apiInfo() {
-//
-//    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fourtrashes.pokerface"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Poker Face",
+                "포커페이스 API DOCS",
+                "1.0 v",
+                "",
+                new Contact("Four Trashes", "https://github.com/HongikDevelopers/poker-face-server", "khs960616@gmail.com"),
+                "License of API", "", Collections.emptyList());
+    }
 }
