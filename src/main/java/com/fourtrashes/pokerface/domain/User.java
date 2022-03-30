eca8d189940679fd6eaed6afb7b983ecd35a2a79
syg <dudrbs7575@naver.com>
2022-03-30 16:16:38 +0900

    feat : [#7] User domain 수정

--- src/main/java/com/fourtrashes/pokerface/domain/User.java
+++ src/main/java/com/fourtrashes/pokerface/domain/User.java
@@ -1,11 +1,15 @@
 package user.example.userCRUD.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -13,12 +17,20 @@ import javax.persistence.*;
 public class User {
 
     @Id
+    @Column(name="userNo")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userNo;
 
+    @Column(name="userId", nullable = false)
     private String userId;
+
+    @Column(name="userPassword", nullable = false)
     private String userPassword;
+
+    @Column(name="userName",nullable = false)
     private String userName;
+
+    @Column(name="money",nullable = false)
     private Long money;
 
     @Enumerated(EnumType.STRING)
