cb143fd8157108d70133c37b36dff41d038efb43
syg <dudrbs7575@naver.com>
2022-03-30 16:34:45 +0900

    feat : [#7] UserRepository 수정

--- src/main/java/com/fourtrashes/pokerface/repository/UserRepository.java
+++ src/main/java/com/fourtrashes/pokerface/repository/UserRepository.java
@@ -1,7 +1,7 @@
-package user.example.userCRUD.repository;
+package com.fourtrashes.pokerface.repository;
 
+import com.fourtrashes.pokerface.domain.User;
 import org.springframework.stereotype.Repository;
-import user.example.userCRUD.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
